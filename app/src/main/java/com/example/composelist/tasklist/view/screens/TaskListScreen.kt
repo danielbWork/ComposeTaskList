e1292ae628fe9b238df1317de66070f5a924669e
milimetron <milimetron@gmail.com>
2022-09-08 12:28:12 +0300

    Fixed Previews
    Todo:
    - Fix dialog ui to actually look good and work for add
    - Add subtasks

--- app/src/main/java/com/example/composelist/tasklist/view/screens/TaskListScreen.kt
+++ app/src/main/java/com/example/composelist/tasklist/view/screens/TaskListScreen.kt
@@ -1,5 +1,7 @@
 package com.example.composelist.tasklist.view.screens
 
+import android.app.Application
+import android.content.res.Configuration
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -23,6 +25,9 @@ import com.example.composelist.tasklist.modal.Task
 import com.example.composelist.tasklist.view.screens.dialogs.DeleteTaskDialog
 import com.example.composelist.tasklist.viewModal.TaskListViewModel
 import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composelist.ui.theme.ComposeListTheme
 
 /**
  * Screen displaying the actual tasks list
@@ -36,25 +41,25 @@ fun TaskListScreen(taskListViewModel: TaskListViewModel) {
 	// Default screen
 	if (tasks.isEmpty()) {
 		Text(
-			text = "No tasks entered yet", textAlign = TextAlign.Center,
-			modifier = Modifier.fillMaxHeight()
+				text = "No tasks entered yet", textAlign = TextAlign.Center,
+				modifier = Modifier.fillMaxHeight()
 		)
 		return
 	}
 
 	val taskToDelete =
-		remember {
-			mutableStateOf<Task?>(
-				null
-			)
-		}// for remembering the value of dialog is open or not
+			remember {
+				mutableStateOf<Task?>(
+						null
+				)
+			}// for remembering the value of dialog is open or not
 
 	if (taskToDelete.value != null) {
 
 		// remove the task item from list
 		DeleteTaskDialog(
-			taskListViewModel = taskListViewModel,
-			taskToDelete = taskToDelete
+				taskListViewModel = taskListViewModel,
+				taskToDelete = taskToDelete
 		)
 	}
 
@@ -66,18 +71,18 @@ fun TaskListScreen(taskListViewModel: TaskListViewModel) {
 
 
 			Card(
-				shape = RoundedCornerShape(20.dp),
-				elevation = 1.dp,
-				modifier = Modifier
-					.padding(5.dp),
-				border = BorderStroke(1.5.dp, MaterialTheme.colors.primary),
+					shape = RoundedCornerShape(20.dp),
+					elevation = 1.dp,
+					modifier = Modifier
+							.padding(5.dp),
+					border = BorderStroke(1.5.dp, MaterialTheme.colors.primary),
 
 
-				) {
+					) {
 				TaskRow(it, { task, _ ->
 
 					taskListViewModel.toggleTaskCompletion(
-						task = task
+							task = task
 					)
 
 				}, {
@@ -99,37 +104,37 @@ fun TaskListScreen(taskListViewModel: TaskListViewModel) {
 fun TaskRow(task: Task, onToggleTask: (Task, Boolean) -> Unit, onDeleteTask: (Task) -> Unit) {
 
 	Row(
-		modifier = Modifier
-			.padding(10.dp)
-			.fillMaxWidth(),
-		verticalAlignment = Alignment.CenterVertically
+			modifier = Modifier
+					.padding(10.dp)
+					.fillMaxWidth(),
+			verticalAlignment = Alignment.CenterVertically
 	) {
 
 
 		Checkbox(
-			checked = task.isComplete, //value of the checkbox return from task list
+				checked = task.isComplete, //value of the checkbox return from task list
 
 
-			onCheckedChange = { isEnabled ->
-				onToggleTask(task, isEnabled)
-			},
+				onCheckedChange = { isEnabled ->
+					onToggleTask(task, isEnabled)
+				},
 		)
 
 
 		Spacer(modifier = Modifier.width(10.dp))
 
 		Text(
-			text = task.name,
-			// check if task item is complete then task will be crossed out
-			textDecoration = if (task.isComplete) {
-				TextDecoration.LineThrough
-			} else {
-				TextDecoration.None
-			},
-			modifier = Modifier.fillMaxWidth(0.7f),
-			style = MaterialTheme.typography.body2,
+				text = task.name,
+				// check if task item is complete then task will be crossed out
+				textDecoration = if (task.isComplete) {
+					TextDecoration.LineThrough
+				} else {
+					TextDecoration.None
+				},
+				modifier = Modifier.fillMaxWidth(0.7f),
+				style = MaterialTheme.typography.body2,
 
-			)
+				)
 
 
 		Spacer(modifier = Modifier.fillMaxSize(0.7f))
@@ -137,39 +142,73 @@ fun TaskRow(task: Task, onToggleTask: (Task, Boolean) -> Unit, onDeleteTask: (Ta
 
 		Icon(
 
-			imageVector = Icons.Filled.Delete, contentDescription = "Delete",
+				imageVector = Icons.Filled.Delete, contentDescription = "Delete",
 
-			modifier = Modifier
-				.clickable {
-					onDeleteTask(task)
-				}
-				.scale(1.2f),
+				modifier = Modifier
+						.clickable {
+							onDeleteTask(task)
+						}
+						.scale(1.2f),
 
 
-			)
+				)
 
 	}
 
 }
 
-//@Preview(name= "Light Mode", showBackground = true,)
-//@Preview(
-//	uiMode = Configuration.UI_MODE_NIGHT_YES,
-//	showBackground = true,
-//	name = "Dark Mode"
-//)
-//@Composable
-//fun TaskListPreview() {
-//
-//	val viewModel = TaskListViewModel(Application())
-//
-//	viewModel.addTask(Task(name ="Test Example", isComplete =  false))
-//	viewModel.addTask(Task(name ="Test Example complete", isComplete = true))
-//	viewModel.addTask(
-//		Task(name ="Test Example complete lonansjfknasjknsakgnjgskanj asndklmsadk",isComplete =  true)
-//	)
-//	ComposeListTheme{
-//		TaskListScreen(taskListViewModel = viewModel)
-//	}
-//}
+
+/**
+ * The preview here is more complex then an average preview to avoid adding the unnecessary preview
+ *  parameter code, while if the card code changes it's not as critical for such a small app
+ */
+@Preview(
+		name = "Light Mode",
+		showBackground = true,
+		device = Devices.PIXEL
+)
+@Preview(
+		uiMode = Configuration.UI_MODE_NIGHT_YES,
+		showBackground = true,
+		name = "Dark Mode",
+		device = Devices.PIXEL
+)
+@Composable
+fun TaskListPreview() {
+
+	val tasks = listOf(
+			Task(name = "Test Example", isComplete = false), Task(
+			name = "Test Example " +
+				   "complete", isComplete = true
+	), Task(
+			name = "Test Example complete lonansjfknasjknsakgnjgskanj asndklmsadk",
+			isComplete = true
+	)
+	)
+
+	ComposeListTheme {
+		// Actual list
+		LazyColumn(modifier = Modifier.fillMaxHeight()) {
+
+			items(items = tasks) {
+				Card(
+						shape = RoundedCornerShape(20.dp),
+						elevation = 1.dp,
+						modifier = Modifier
+								.padding(5.dp),
+						border = BorderStroke(1.5.dp, MaterialTheme.colors.primary),
+
+
+						) {
+					TaskRow(
+							task = it,
+							onToggleTask = { _: Task, _: Boolean -> },
+							onDeleteTask = { }
+					)
+				}
+			}
+
+		}
+	}
+}
 
